b28967be33b7f67d28d071fb66dcdae82673ccdb
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-03 03:27:54 +0200

    Add open/close script

--- src/app.js
+++ src/app.js
@@ -1,2 +1,28 @@
 import '@mdi/font/css/materialdesignicons.css'
 import './styles.css';
+
+const primaryNav = document.querySelector('.primary-nav');
+const openPrimaryNavButton = document.querySelector('.open.primary-nav-button');
+const closePrimaryNavButton = document.querySelector('.close.primary-nav-button');
+
+openPrimaryNavButton.addEventListener('click', (e) => {
+  openPrimaryNavButton.disabled = true;
+  closePrimaryNavButton.disabled = false;
+  primaryNav.style.left = '0px';
+});
+
+closePrimaryNavButton.addEventListener('click', (e) => {
+  openPrimaryNavButton.disabled = false;
+  closePrimaryNavButton.disabled = true;
+  primaryNav.style.left = '';
+});
+
+primaryNav.querySelectorAll('li:not(.section)').forEach((navItem) => {
+  navItem.addEventListener('click', (e) => {
+    const currentSelected = primaryNav.querySelector('li.current');
+    if (navItem === currentSelected) return;
+
+    currentSelected.classList.remove('current');
+    navItem.classList.add('current');
+  });
+});
