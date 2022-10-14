d8252785c8a3859ecc3c4259155bf8fc38fa9063
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-14 13:00:01 +0200

    Fix constructor and add date get/set methods

--- src/scripts/DatePicker.js
+++ src/scripts/DatePicker.js
@@ -1,31 +1,60 @@
-import { compareAsc, format } from "date-fns";
+import { compareAsc, format, max } from "date-fns";
 
 export default class {
   static DATE_FORMAT = 'yyyy-MM-dd';
   #datePicker;
+  #minDate;
+  #maxDate;
 
   constructor(datePickerElement, date, minDate, maxDate) {
     this.#datePicker = datePickerElement;
-    
-    if (date) {
-      datePickerElement.value = format(new Date(), this.constructor.DATE_FORMAT);
-    }
+    this.#minDate = minDate;
+    this.#maxDate = maxDate;
 
     if (minDate) {
-      datePickerElement.min = format(new Date(), this.constructor.DATE_FORMAT);
+      datePickerElement.min = format(minDate, this.constructor.DATE_FORMAT);
     }
 
     if (maxDate) {
-      datePickerElement.max = format(new Date(), this.constructor.DATE_FORMAT);
+      datePickerElement.max = format(maxDate, this.constructor.DATE_FORMAT);
     }
 
-    datePickerElement.addEventListener('focusout', (e) => {
-      if (compareAsc(new Date(datePickerElement.value), minDate) >= 0) return;
-      datePickerElement.value = format(minDate, this.constructor.DATE_FORMAT);
-    });
+    if (!this.#validate(date)) {
+      datePickerElement.value = format(date, this.constructor.DATE_FORMAT);
+    }
+
+    datePickerElement.addEventListener('focusout', (e) => this.#validate(new Date(e.target.value)));
+  }
+
+  #validate(date) {
+    if (compareAsc(date, this.#minDate) < 0) {
+      this.#datePicker.value = format(this.#minDate, this.constructor.DATE_FORMAT);
+      return -1;
+    }
+
+    if (compareAsc(date, this.#maxDate) > 0) {
+      this.#datePicker.value = format(this.#maxDate, this.constructor.DATE_FORMAT);
+      return 1;
+    }
+
+    return 0;
   }
 
   get value() {
     return this.#datePicker.value;
+  }
+
+  set value(value) {
+    if (this.#validate(new Date(value))) return;
+    this.#datePicker.value = value;
+  }
+
+  setDate(date) {
+    if (this.#validate(date)) return;
+    this.#datePicker.value = format(date, this.constructor.DATE_FORMAT);
+  }
+
+  getDate() {
+    return new Date(this.#datePicker.value);
   }
 }
