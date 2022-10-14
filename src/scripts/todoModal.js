f2f02b76069cb6b9381956b9b49f91590e421d79
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-14 13:07:03 +0200

    Add confirm event

--- src/scripts/todoModal.js
+++ src/scripts/todoModal.js
@@ -1,5 +1,13 @@
+import DatePicker from "./DatePicker";
+import RadioList from "./RadioList";
+
 const todoModal = document.querySelector('.todo-modal');
 export default todoModal;
+
+const title = todoModal.querySelector('.title');
+const description = todoModal.querySelector('.description');
+const priorityList = new RadioList(todoModal.querySelector('.priority-choice-list'));
+const dueDatePicker = new DatePicker(todoModal.querySelector('.due-date-picker'), new Date(2020, 11, 1), new Date());
 
 const showEvent = new Event('showTodoModal');
 const backEvent = new Event('backTodoModal');
@@ -12,9 +20,26 @@ todoModal.show = function (input) {
 }
 
 const backButton = todoModal.querySelector('button.back');
-
-
 backButton.addEventListener('click', (e) => {
   todoModal.style.display = 'none';
   todoModal.dispatchEvent(backEvent);
 });
+
+const confirmButton = todoModal.querySelector('button.confirm');
+confirmButton.addEventListener('click', (e) => {
+  const result = {
+    title: title.value,
+    description: description.value,
+    priority: priorityList.value,
+    dueDate: dueDatePicker.value,
+  };
+
+  reset();
+  backButton.click();
+  todoModal.dispatchEvent(new CustomEvent('confirmTodoModal', { detail: result }));
+});
+
+function reset() {
+  priorityList.reset();
+  dueDatePicker.setDate(new Date());
+}
