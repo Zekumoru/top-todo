8a8c98b9b479574baa769e48e1bbf9c203edb9b1
Zekumoru Dragonhart <dev@zekumoru.com>
2022-10-22 18:37:38 +0200

    Add create input interactability

--- src/scripts/Modal/ProjectModal.js
+++ src/scripts/Modal/ProjectModal.js
@@ -2,9 +2,18 @@ import Sortable from 'sortablejs';
 import Modal from './Modal';
 
 export default class extends Modal {
+  #createInput;
+  #createButton;
+  #enterButton;
+
   constructor(element) {
     super(element);
+    this.#createInput = element.querySelector('.create-input');
+    this.#createButton = element.querySelector('button.create');
+    this.#enterButton = element.querySelector('button.enter');
     this.#setProjectListSortability();
+    this.#setCreateInputEvents();
+    this.#setCreateButtonEvents();
   }
 
   #setProjectListSortability() {
@@ -14,4 +23,43 @@ export default class extends Modal {
       handle: '.drag-handle',
     });
   }
-}
+
+  #setCreateInputEvents() {
+    this.#createInput.addEventListener('focus', () => {
+      this.#createButton.classList.remove('mdi-plus');
+      this.#createButton.classList.add('mdi-close');
+    });
+
+    this.#createInput.addEventListener('keyup', () => {
+      if (!this.#createInput.value) {
+        this.#enterButton.style.display = 'none';
+        return;
+      }
+
+      this.#enterButton.style.display = 'block';
+    });
+  }
+
+  #setCreateButtonEvents() {
+    this.element.addEventListener('click', (e) => {
+      if (e.target === this.#createInput) return;
+      if (document.activeElement === this.#createInput) return;
+      if (this.#createInput.value) return;
+      if (this.#createButton.classList.contains('mdi-plus')) return;
+      this.#createButton.classList.remove('mdi-close');
+      this.#createButton.classList.add('mdi-plus');
+    });
+
+    this.#createButton.addEventListener('click', () => {
+      if (this.#createButton.classList.contains('mdi-close')) {
+        this.#createButton.classList.remove('mdi-close');
+        this.#createButton.classList.add('mdi-plus');
+        this.#enterButton.style.display = 'none';
+        this.#createInput.value = '';
+        return;
+      }
+      
+      this.#createInput.focus();
+    });
+  }
+};
