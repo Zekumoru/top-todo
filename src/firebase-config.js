840caa8700f85828b88d4f8beb7a7a116f77e562
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-05 21:00:02 +0100

    Init firebase and add firebase config js file

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyCpz8idx6poCMJxER93ds87vXXgBfNZ_vk",
+  authDomain: "keedo-todos.firebaseapp.com",
+  projectId: "keedo-todos",
+  storageBucket: "keedo-todos.appspot.com",
+  messagingSenderId: "883342083756",
+  appId: "1:883342083756:web:b435bf467854943029c109"
+}
+
+export default config;
